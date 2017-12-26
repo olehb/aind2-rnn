cec47a763f14e5a847b305f1420c913a3995a337
Oleg Boiko <oleh.boiko@gmail.com>
2017-12-25 17:25:43 -0800

    Implementing batching

--- my_answers.py
+++ my_answers.py
@@ -9,15 +9,13 @@ import keras
 # TODO: fill out the function below that transforms the input series 
 # and window-size into a set of input/output pairs for use with our RNN model
 def window_transform_series(series, window_size):
-    # containers for input/output pairs
-    X = []
-    y = []
+    rows = series.size-window_size
+    X = np.zeros((rows, window_size))
+    y = np.zeros((rows, 1))
 
-    # reshape each 
-    X = np.asarray(X)
-    X.shape = (np.shape(X)[0:2])
-    y = np.asarray(y)
-    y.shape = (len(y),1)
+    for i in range(rows):
+        X[i] = series[i:i+window_size]
+        y[i] = series[i+window_size]
 
     return X,y
 
